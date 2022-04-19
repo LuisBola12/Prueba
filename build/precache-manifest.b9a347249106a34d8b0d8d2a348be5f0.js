c412dfc15dea4728c2e9242063b646b6fd16bd76
LuisBola12 <luibv1202@gmail.com>
2022-04-19 09:41:32 -0600

    Prueba 2

---
+++ build/precache-manifest.b9a347249106a34d8b0d8d2a348be5f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c709620eb607b507e97",
+    "url": "/static/css/main.c7224a6b.chunk.css"
+  },
+  {
+    "revision": "7c709620eb607b507e97",
+    "url": "/static/js/main.7c709620.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "413b728f6bda178a29fd",
+    "url": "/static/js/2.413b728f.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "02e07a8d5a303c1cfff171ed8f63cd82",
+    "url": "/index.html"
+  }
+];
